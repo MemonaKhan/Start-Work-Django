79e67cbcc0783c748eb76aa60f9c73558c273aca
MemonaKhan <suhail1975pk@yahoo.co.uk>
2021-02-18 16:19:23 +0500

    done initial confg

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', include("firstapp.urls")),
-    path('newyear/', include("newyearapp.urls"))
+    path('newyear/', include("newyearapp.urls")),
+    path('todo/', include("taskstodo.urls")),
 ]
