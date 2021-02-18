ee7c169d09aeb2ad20017dfa1e19affe42fd7ddd
MemonaKhan <suhail1975pk@yahoo.co.uk>
2021-02-18 16:50:56 +0500

    done inheriting code

--- mysite/taskstodo/views.py
+++ mysite/taskstodo/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+tasks = ["abc","foo","bar"]
+def index(request):
+    return render(request,"taskstodo/index.html",{
+        "tasks":tasks
+    })
+
+def add(request):
+    return render(request,"taskstodo/add.html")
